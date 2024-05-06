8a87b82ee112f62dd946a61e8f836db8601cfed2
IsaUrie <isa-bela@ufmg.br>
2024-05-05 23:38:32 -0300

    initial commit

---
+++ src/main/java/br/com/isabela/TabelaFipe/model/DadosCarro.java
@@ -0,0 +1,24 @@
+package br.com.isabela.TabelaFipe.model;
+
+public class DadosCarro {
+    private String marca;
+    private String modelo;
+    private Integer ano;
+    private String valor;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
---
+++ src/main/java/br/com/isabela/TabelaFipe/principal/Principal.java
@@ -0,0 +1,5 @@
+package br.com.isabela.TabelaFipe.principal;
+
+public class Principal {
+
+}
